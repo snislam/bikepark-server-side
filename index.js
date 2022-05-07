1e100639d85a341c152fcc4d03bb40864e56e3dc
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-05-07 19:26:10 +0600

    Server set with mongo and items api

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 
+
+// bikeuser1
+// Ncm9Jqk55kUA04Fe
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+// mongodbconnection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajbho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('connected')
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+async function run() {
+    try {
+        await client.connect()
+        const bikeCollection = client.db("bikePark").collection("bikeCollection")
+        console.log("connected")
+
+        // get all bike items api
+        app.get('/bikeitems', async (req, res) => {
+            const query = {};
+            const cursor = bikeCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 // general api
 app.get('/', (req, res) => {
